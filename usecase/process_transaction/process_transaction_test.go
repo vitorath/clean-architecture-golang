bb58bf3dc5faf8b996973dd1659f5498b0d1722e
Vitor Thomazini <vitor.thomazini94@gmail.com>
2021-12-04 17:42:38 -0300

    test(transation process): ensure process transaction its invalid

--- usecase/process_transaction/process_transaction_test.go
+++ usecase/process_transaction/process_transaction_test.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -27,6 +28,32 @@ func TestProcessTransactionWhenItsValid(t *testing.T) {
 	processTransaction := NewProcessTransaction(repositoryMock)
 
 	repositoryMock.EXPECT().Insert(input.ID, input.AccountID, input.Amount, "approved", "").Return(nil)
+
+	output, err := processTransaction.Execute(input)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedOutput, output)
+}
+
+func TestProcessTransactionWhenItsInvalid(t *testing.T) {
+	input := TransactionDtoInput{
+		ID:        "1",
+		AccountID: "1",
+		Amount:    1001,
+	}
+
+	expectedOutput := TransactionDtoOutput{
+		ID:           "1",
+		Status:       "rejected",
+		ErrorMessage: "you don't have limit for this transaction",
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	repositoryMock := mock_entity.NewMockTransactionRepository(ctrl)
+	processTransaction := NewProcessTransaction(repositoryMock)
+
+	errorMessage := errors.New("you don't have limit for this transaction")
+	repositoryMock.EXPECT().Insert(input.ID, input.AccountID, input.Amount, "rejected", errorMessage.Error()).Return(nil)
 
 	output, err := processTransaction.Execute(input)
 	assert.Nil(t, err)
