6f0da1173d49c5faded55cf7e0cf5d06537a65f8
Vitor Thomazini <vitor.thomazini94@gmail.com>
2021-12-04 17:54:06 -0300

    refactor(process transaction): extract SUT for tests

--- usecase/process_transaction/process_transaction_test.go
+++ usecase/process_transaction/process_transaction_test.go
@@ -9,7 +9,7 @@ import (
 	mock_entity "github.com/vitorath/clean-architecture-golang/entity/mock"
 )
 
-func TestProcessTransactionWhenItsValid(t *testing.T) {
+func newSut(t *testing.T) (TransactionDtoInput, TransactionDtoOutput, *ProcessTransaction, *mock_entity.MockTransactionRepository) {
 	input := TransactionDtoInput{
 		ID:        "1",
 		AccountID: "1",
@@ -26,7 +26,11 @@ func TestProcessTransactionWhenItsValid(t *testing.T) {
 	defer ctrl.Finish()
 	repositoryMock := mock_entity.NewMockTransactionRepository(ctrl)
 	processTransaction := NewProcessTransaction(repositoryMock)
+	return input, expectedOutput, processTransaction, repositoryMock
+}
 
+func TestProcessTransactionWhenItsValid(t *testing.T) {
+	input, expectedOutput, processTransaction, repositoryMock := newSut(t)
 	repositoryMock.EXPECT().Insert(input.ID, input.AccountID, input.Amount, "approved", "").Return(nil)
 
 	output, err := processTransaction.Execute(input)
@@ -35,23 +39,10 @@ func TestProcessTransactionWhenItsValid(t *testing.T) {
 }
 
 func TestProcessTransactionWhenItsInvalid(t *testing.T) {
-	input := TransactionDtoInput{
-		ID:        "1",
-		AccountID: "1",
-		Amount:    1001,
-	}
-
-	expectedOutput := TransactionDtoOutput{
-		ID:           "1",
-		Status:       "rejected",
-		ErrorMessage: "you don't have limit for this transaction",
-	}
-
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	repositoryMock := mock_entity.NewMockTransactionRepository(ctrl)
-	processTransaction := NewProcessTransaction(repositoryMock)
-
+	input, expectedOutput, processTransaction, repositoryMock := newSut(t)
+	input.Amount = 1001
+	expectedOutput.Status = "rejected"
+	expectedOutput.ErrorMessage = "you don't have limit for this transaction"
 	errorMessage := errors.New("you don't have limit for this transaction")
 	repositoryMock.EXPECT().Insert(input.ID, input.AccountID, input.Amount, "rejected", errorMessage.Error()).Return(nil)
 
