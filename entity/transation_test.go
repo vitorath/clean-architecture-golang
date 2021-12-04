4b05240b42182a38d3ed6845cc5076e3d5db9e11
Vitor Thomazini <vitor.thomazini94@gmail.com>
2021-12-04 16:16:01 -0300

    test(transation): ensure IsValid return error when amount lesser than 1

--- entity/transation_test.go
+++ entity/transation_test.go
@@ -16,3 +16,13 @@ func TestTransactionWithAmountGreaterThan1000(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, "you don't have limit for this transaction", err.Error())
 }
+
+func TestTransactionWithAmountLesserThan1(t *testing.T) {
+	transaction := entity.NewTransaction()
+	transaction.ID = "1"
+	transaction.AccountID = "1"
+	transaction.Amount = 0
+	err := transaction.IsValid()
+	assert.Error(t, err)
+	assert.Equal(t, "the amount must be greater than 1", err.Error())
+}
