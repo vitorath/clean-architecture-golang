20b68fd9e31caba30c2c7ac510f03420d832c649
Vitor Thomazini <vitor.thomazini94@gmail.com>
2021-12-04 19:27:47 -0300

    test(transaction repository): ensure insert transaction

---
+++ adapter/repository/transaction_repository_db_test.go
@@ -0,0 +1,20 @@
+package repository
+
+import (
+	"os"
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+	"github.com/vitorath/clean-architecture-golang/adapter/repository/fixtures"
+)
+
+func TestTransactionRepositoryDbInsert(t *testing.T) {
+	migrationsDir := os.DirFS("fixtures/sql")
+	db := fixtures.Up(migrationsDir)
+	defer fixtures.Down(db, migrationsDir)
+
+	repository := NewTransactionRepositoryDb(db)
+	err := repository.Insert("1", "1", 2, "approved", "")
+	assert.Nil(t, err)
+}
